baf302ff3b492f5a085b09eccc259719f442b4b9
shareque <shareque@reichprinz.com>
2019-02-08 17:13:24 +0530

    temperature commit

--- app/src/main/java/activity/rprs/com/daycaremain/TemperatureFragment.java
+++ app/src/main/java/activity/rprs/com/daycaremain/TemperatureFragment.java
@@ -1,21 +1,56 @@
 package activity.rprs.com.daycaremain;
 
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class TemperatureFragment extends Fragment implements View.OnClickListener{
@@ -33,10 +68,19 @@ public class TemperatureFragment extends Fragment implements View.OnClickListene
     Context context;
     String fTime,fDate,fDate2;
     private int  mHour, mMinute;
-    String format;
-    ImageView edittimeimg;
+    String format,progressvalue;
+    ImageView edittimeimg,pickimg,imageViewset;
+    EditText editoption;
+    Boolean checkbtn=true;
 
-    Button celbtn,fahbtn;
+    Button celbtn,fahbtn,applybtn;
+    Spinner spillnes,spstatus;
+
+    Bitmap bitmap;
+    ProgressDialog progressDialog,dialog;
+    String ServerUploadPath ="http://reichprinz.com/teaAndroid/Daycare/addmedicineimg.php";
+    String url="http://reichprinz.com/teaAndroid/Daycare/addmedicine.php";
+    boolean check = true;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -76,15 +120,21 @@ public class TemperatureFragment extends Fragment implements View.OnClickListene
 
         context=getActivity();
         appbartextv=(TextView)getActivity().findViewById(R.id.maintextv);
-        appbartextv.setText("Temperature");
+        appbartextv.setText("Medicine/Temperature");
 
         textViewtime=(TextView)view.findViewById(R.id.textViewmtime);
 
+        editoption=(EditText)view.findViewById(R.id.editText4);
+
         celbtn=(Button)view.findViewById(R.id.button8);
         fahbtn=(Button)view.findViewById(R.id.button9);
+        applybtn=(Button)view.findViewById(R.id.button2);
+        spillnes=(Spinner)view.findViewById(R.id.spinner3);
+        spstatus=(Spinner)view.findViewById(R.id.spinner4);
 
         celbtn.setOnClickListener(this);
         fahbtn.setOnClickListener(this);
+        applybtn.setOnClickListener(this);
 
         seekBar=(SeekBar)view.findViewById(R.id.seekBar);
         progresstext=(TextView)view.findViewById(R.id.textView20);
@@ -93,7 +143,14 @@ public class TemperatureFragment extends Fragment implements View.OnClickListene
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                progresstext.setText(String.valueOf(i)+" "+ (char) 0x00B0 +"C");
+                if (checkbtn) {
+                    progresstext.setText(String.valueOf(i) + " " + (char) 0x00B0 + "C");
+                    progressvalue=String.valueOf(i) + " C" ;
+                }
+                else {
+                    progresstext.setText(String.valueOf(i) + " " + (char) 0x00B0 + "F");
+                    progressvalue=String.valueOf(i) + " F" ;
+                }
             }
 
             @Override
@@ -108,7 +165,10 @@ public class TemperatureFragment extends Fragment implements View.OnClickListene
         });
 
         edittimeimg=(ImageView) view.findViewById(R.id.imageView2);
+        pickimg=(ImageView) view.findViewById(R.id.imageView4);
         edittimeimg.setOnClickListener(this);
+        pickimg.setOnClickListener(this);
+        imageViewset=(ImageView)view.findViewById(R.id.imageView25);
 
         cal = Calendar.getInstance();
         mHour = cal.get(Calendar.HOUR_OF_DAY);
@@ -130,15 +190,58 @@ public class TemperatureFragment extends Fragment implements View.OnClickListene
             case R.id.button8:
                 celbtn.setBackground(getResources().getDrawable(R.drawable.my_butfull));
                 fahbtn.setBackground(getResources().getDrawable(R.drawable.my_buttonblue));
+                seekBar.setMax(41);
+                seekBar.setProgress(38);
+                progresstext.setText(38+" "+ (char) 0x00B0 +"C");
+                checkbtn=true;
                 break;
 
             case R.id.button9:
                 fahbtn.setBackground(getResources().getDrawable(R.drawable.my_butfull));
                 celbtn.setBackground(getResources().getDrawable(R.drawable.my_buttonblue));
+                seekBar.setMax(105);
+                seekBar.setProgress(101);
+                progresstext.setText(101+" "+ (char) 0x00B0 +"F");
+                checkbtn=false;
                 break;
             case R.id.imageView2:
                 setTime();
                 break;
+
+            case R.id.button2:
+                String optext="";
+                if (editoption.getText().toString().trim().length()>0){
+                    optext=editoption.getText().toString();
+                }
+
+                if (bitmap==null){
+                    saveData(optext);
+                }else {
+                    ImageUploadToServerFunction(optext);
+                }
+                break;
+
+            case R.id.imageView4:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Image From Gallery"), 1);
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
+                bitmap=getResizedBitmap(bitmap,500);
+                imageViewset.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -178,6 +281,184 @@ public class TemperatureFragment extends Fragment implements View.OnClickListene
             }
         }, mHour, mMinute,false);
         timePickerDialog.show();
+    }
+
+    public void saveData(final String optext){
+        dialog = new ProgressDialog(context);
+        dialog.setMessage("Please Wait....");
+        dialog.setCancelable(false);
+        dialog.show();
+        StringRequest request = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String string) {
+                dialog.dismiss();
+                //Toast.makeText(context, "data "+string, Toast.LENGTH_SHORT).show();
+                if (string.equals("1")){
+                    getActivity().finish();
+                    //Intent intent=new Intent(context,)
+                }else {
+                    Toast.makeText(context, "Please Try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                dialog.dismiss();
+                //Toast.makeText(context,volleyError.toString(), Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setMessage("Something went wrong Check your Connection !");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNeutralButton("Retry", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        saveData(optext);
+                    }
+                });
+                alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getActivity().finish();
+                    }
+                });
+                alertDialogBuilder.show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("date", fDate);
+                params.put("Stud_Id", DataClass.Stud_id);
+                params.put("disease", spillnes.getSelectedItem().toString());
+                params.put("status", spstatus.getSelectedItem().toString());
+                params.put("temperature", progressvalue);
+                params.put("mdname", optext);
+                params.put("notiftext", "Medical emergency "+spillnes.getSelectedItem().toString());
+                params.put("time", mHour+":"+mMinute);
+                return params;
+            }
+        };
+
+        RequestQueue rQueue = Volley.newRequestQueue(context);
+        rQueue.add(request);
+    }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
+    public void ImageUploadToServerFunction(final String optext){
+
+        ByteArrayOutputStream byteArrayOutputStreamObject ;
+        byteArrayOutputStreamObject = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStreamObject);
+        byte[] byteArrayVar = byteArrayOutputStreamObject.toByteArray();
+        final String ConvertImage = Base64.encodeToString(byteArrayVar, Base64.DEFAULT);
+        class AsyncTaskUploadClass extends AsyncTask<Void,Void,String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = ProgressDialog.show(context,"Image is Uploading..","Please Wait",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String string1) {
+                super.onPostExecute(string1);
+                // Dismiss the progress dialog after done uploading.
+                progressDialog.dismiss();
+                // Printing uploading success message coming from server on android app.
+                if (string1.equals("1"))
+                {
+                    getActivity().finish();
+                }else {
+                    Toast.makeText(context,"Please Try again",Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            protected String doInBackground(Void... params) {
+                TemperatureFragment.ImageProcessClass imageProcessClass = new TemperatureFragment.ImageProcessClass();
+                HashMap<String,String> HashMapParams = new HashMap<String,String>();
+                HashMapParams.put("date", fDate);
+                HashMapParams.put("Stud_Id", DataClass.Stud_id);
+                HashMapParams.put("disease", spillnes.getSelectedItem().toString());
+                HashMapParams.put("status", spstatus.getSelectedItem().toString());
+                HashMapParams.put("temperature", progressvalue);
+                HashMapParams.put("mdname", optext);
+                HashMapParams.put("notiftext", "Medical emergency "+spillnes.getSelectedItem().toString());
+                HashMapParams.put("time", mHour+":"+mMinute);
+                HashMapParams.put("image_path", ConvertImage);
+                HashMapParams.put("image_name", DataClass.Stud_id+fDate2);
+                String FinalData = imageProcessClass.ImageHttpRequest(ServerUploadPath, HashMapParams);
+                return FinalData;
+            }
+        }
+        AsyncTaskUploadClass AsyncTaskUploadClassOBJ = new AsyncTaskUploadClass();
+        AsyncTaskUploadClassOBJ.execute();
+    }
+
+    public class ImageProcessClass{
+
+        public String ImageHttpRequest(String requestURL,HashMap<String, String> PData) {
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                URL url;
+                HttpURLConnection httpURLConnectionObject ;
+                OutputStream OutPutStream;
+                BufferedWriter bufferedWriterObject ;
+                BufferedReader bufferedReaderObject ;
+                int RC ;
+                url = new URL(requestURL);
+                httpURLConnectionObject = (HttpURLConnection) url.openConnection();
+                httpURLConnectionObject.setReadTimeout(19000);
+                httpURLConnectionObject.setConnectTimeout(19000);
+                httpURLConnectionObject.setRequestMethod("POST");
+                httpURLConnectionObject.setDoInput(true);
+                httpURLConnectionObject.setDoOutput(true);
+                OutPutStream = httpURLConnectionObject.getOutputStream();
+                bufferedWriterObject = new BufferedWriter(
+                        new OutputStreamWriter(OutPutStream, "UTF-8"));
+                bufferedWriterObject.write(bufferedWriterDataFN(PData));
+                bufferedWriterObject.flush();
+                bufferedWriterObject.close();
+                OutPutStream.close();
+                RC = httpURLConnectionObject.getResponseCode();
+                if (RC == HttpsURLConnection.HTTP_OK) {
+                    bufferedReaderObject = new BufferedReader(new InputStreamReader(httpURLConnectionObject.getInputStream()));
+                    stringBuilder = new StringBuilder();
+                    String RC2;
+                    while ((RC2 = bufferedReaderObject.readLine()) != null){
+                        stringBuilder.append(RC2);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return stringBuilder.toString();
+        }
+        private String bufferedWriterDataFN(HashMap<String, String> HashMapParams) throws UnsupportedEncodingException {
+            StringBuilder stringBuilderObject;
+            stringBuilderObject = new StringBuilder();
+            for (Map.Entry<String, String> KEY : HashMapParams.entrySet()) {
+                if (check)
+                    check = false;
+                else
+                    stringBuilderObject.append("&");
+                stringBuilderObject.append(URLEncoder.encode(KEY.getKey(), "UTF-8"));
+                stringBuilderObject.append("=");
+                stringBuilderObject.append(URLEncoder.encode(KEY.getValue(), "UTF-8"));
+            }
+            return stringBuilderObject.toString();
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
