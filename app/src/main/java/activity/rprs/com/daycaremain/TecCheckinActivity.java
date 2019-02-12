0a5bd0bfdf3a1397ff39b69e79c60a193c940318
shareque <shareque@reichprinz.com>
2019-02-12 18:07:18 +0530

    edit commit

---
+++ app/src/main/java/activity/rprs/com/daycaremain/TecCheckinActivity.java
@@ -0,0 +1,13 @@
+package activity.rprs.com.daycaremain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TecCheckinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tec_checkin);
+    }
+}
